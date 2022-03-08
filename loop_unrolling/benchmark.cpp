3a91f7bf9688c1bf21d37fe1325dcbeb9b97d42a
Gordon <gordonl0811@gmail.com>
2022-03-08 23:02:13 +0000

    Created function generating a vector of random numbers. Created CMakeList for loop unrolling.

--- loop_unrolling/benchmark.cpp
+++ loop_unrolling/benchmark.cpp
@@ -2,27 +2,46 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
 
+#include <vector>
+#include <random>
+
 using namespace std;
+
+static vector<int> generateRandomInts(int size) {
+
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(1, 100);
+
+    vector<int> ints;
+    ints.reserve(size);
+
+    for (int i = 0; i < size; i++) {
+        ints.push_back(dist(mt));
+    }
+
+    return ints;
+}
 
 static void benchmarkSimpleLoop(benchmark::State& state) {
     for (auto _ : state) {
-        for (auto i = 0; i < state.range(0); i++) {
-            auto zippedVector = LoopUnrolling::SimpleLoop();
-            benchmark::DoNotOptimize(zippedVector);
-        }
+        int size = state.range(0);
+        auto zippedVector = LoopUnrolling::SimpleLoop(generateRandomInts(size), generateRandomInts(size), size);
+        benchmark::DoNotOptimize(zippedVector);
     }
 }
 
 static void benchmarkSimpleLoopUnrolled(benchmark::State& state) {
     for (auto _ : state) {
         for (auto i = 0; i < state.range(0); i++) {
-            auto zippedVector = LoopUnrolling::SimpleLoopUnrolled();
+            int size = state.range(0);
+            auto zippedVector = LoopUnrolling::SimpleLoopUnrolled(generateRandomInts(size), generateRandomInts(size), size);
             benchmark::DoNotOptimize(zippedVector);
         }
     }
 }
 
-BENCHMARK(benchmarkInlining)->RangeMultiplier(10)->Range(1, 10000000);
-BENCHMARK(benchmarkNoInlining)->RangeMultiplier(10)->Range(1, 10000000);
+BENCHMARK(benchmarkSimpleLoop)->RangeMultiplier(10)->Range(1, 10000);
+BENCHMARK(benchmarkSimpleLoopUnrolled)->RangeMultiplier(10)->Range(1, 10000);
 
 BENCHMARK_MAIN();
