7638f4dddc4db0df5c703c368d9e1e367e7324aa
gordonl0811 <gordonl0811@gmail.com>
2022-04-20 14:08:35 +0100

    Fixed #include <utils> for benchmarks

--- examples/predication/benchmark.cpp
+++ examples/predication/benchmark.cpp
@@ -1,7 +1,7 @@
 #include <benchmark/benchmark.h>
 #include "predication.h"
 
-#include "../../utils/utils.h"
+#include "../utils/utils.h"
 
 static void BenchmarkTrimVector(benchmark::State& state) {
 
