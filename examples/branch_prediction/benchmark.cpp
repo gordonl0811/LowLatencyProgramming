ae44f9a1e84c6765e17f7b98d868c18bbb1d4d0a
gordonl0811 <gordonl0811@gmail.com>
2022-04-22 15:13:04 +0100

    Reworked Branch Prediction to use CountEven function

--- examples/branch_prediction/benchmark.cpp
+++ examples/branch_prediction/benchmark.cpp
@@ -2,22 +2,29 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
 
-static void BenchmarkNoBranchPrediction(benchmark::State& state) {
+static void BenchmarkCountEvens(benchmark::State& state) {
+
+    std::vector<int> someEvens = BranchPrediction::GenerateSomeEvens((float) state.range(0), 1000000, 100);
+
     for (auto _ : state) {
-        int res = BranchPrediction::IsSeven(7);
-        benchmark::DoNotOptimize(res);
+        benchmark::DoNotOptimize(BranchPrediction::CountEvens(someEvens));
     }
 }
 
-static void BenchmarkBranchPrediction(benchmark::State& state) {
+static void BenchmarkCountEvensLikely(benchmark::State& state) {
+
+    std::vector<int> someEvens = BranchPrediction::GenerateSomeEvens((float) state.range(0), 1000000, 100);
+
     for (auto _ : state) {
-        int res = BranchPrediction::IsSevenPredicted(7);
-        benchmark::DoNotOptimize(res);
+        benchmark::DoNotOptimize(BranchPrediction::CountEvensLikely(someEvens));
     }
 }
 
 
-BENCHMARK(BenchmarkNoBranchPrediction)->RangeMultiplier(10)->Range(1, 10000000);
-BENCHMARK(BenchmarkBranchPrediction)->RangeMultiplier(10)->Range(1, 10000000);
+//BENCHMARK(BenchmarkNoBranchPrediction)->RangeMultiplier(10)->Range(1, 10000000);
+//BENCHMARK(BenchmarkBranchPrediction)->RangeMultiplier(10)->Range(1, 10000000);
+
+BENCHMARK(BenchmarkCountEvens)->DenseRange(0, 100, 10);
+BENCHMARK(BenchmarkCountEvensLikely)->DenseRange(0, 100, 10);
 
 BENCHMARK_MAIN();
--- examples/branch_prediction/branch_prediction.h
+++ examples/branch_prediction/branch_prediction.h
@@ -1,26 +1,64 @@
 #ifndef LOW_LATENCY_MICROBENCHMARKS_BRANCH_PREDICTION_H
 #define LOW_LATENCY_MICROBENCHMARKS_BRANCH_PREDICTION_H
 
+#include <vector>
+#include <random>
+
 class BranchPrediction {
 
 public:
 
-    static int IsSeven(int num) {
-        if (num == 7) {
-            return num;
-        } else {
-            return -1;
+    static std::vector<int> GenerateSomeEvens(float evenPercentage, int size, int range) {
+
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_real_distribution<double> distIntegers(1, range / 2);
+        std::uniform_real_distribution<float> distProbability(0, 1);
+
+        std::vector<int> someEvens = std::vector<int>(size);
+
+        for (int i = 0; i < size; i++) {
+
+            // Generate random n
+            int n = (int) distIntegers(mt);
+
+            // Return an even or odd number depending on the generated value
+            if (distProbability(mt) * 100 < evenPercentage) {
+                someEvens[i] = 2 * n;
+            } else {
+                someEvens[i] = 2 * n - 1;
+            }
+
         }
+
+        return someEvens;
 
     }
 
-    static int IsSevenPredicted(int num) {
+    static int CountEvens(std::vector<int> &nums) {
 
-        if (__builtin_expect(num == 7, 0)) {
-            return num;
-        } else {
-            return -1;
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 == 0) {
+                count++;
+            }
         }
+
+        return count;
+    }
+
+    static int CountEvensLikely(std::vector<int> &nums) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (__builtin_expect((nums[i] % 2 == 0), 0)) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
 };
