72617e8601778014a7f5fd242a78bb26f6d3556a
Gordon <gl119@ic.ac.uk>
2022-05-28 17:54:03 +0100

    Introduced random generation of TCP/UDP packets at a 50-50 chance.

--- packet-processor/pcap_generator/pcap_generator.py
+++ packet-processor/pcap_generator/pcap_generator.py
@@ -1,7 +1,7 @@
 from scapy.all import wrpcap
-from scapy.layers.inet import Ether, IP, TCP
+from scapy.layers.inet import Ether, IP, TCP, UDP
 
-from random import randint
+from random import randint, uniform
 
 
 def generate_tcp_mss_packet(min_mss, max_mss):
@@ -11,16 +11,34 @@ def generate_tcp_mss_packet(min_mss, max_mss):
     return packet
 
 
+def generate_udp_packet():
+    return Ether() / IP()/ UDP()
+
+
 def generate_random_packets(output_file, packet_count):
 
+    tcp_count = 0
+
     for i in range(packet_count):
+
+        tcp_chance = uniform(0, 1)
+
+        if tcp_chance > 0.5:
+            tcp_count += 1
+            packet = generate_tcp_mss_packet(1400, 1600)
+        else:
+            packet = generate_udp_packet()
+
         wrpcap(
             output_file,
-            generate_tcp_mss_packet(1400, 1600),
+            packet,
             append=True
         )
 
+    print(f"TCP Packets: {tcp_count}")
+    print(f"UDP Packets: {packet_count - tcp_count}")
+
 
 if __name__ == "__main__":
-    # generate_random_packets("input.pcap", 1000)
-    generate_random_packets("single_input.pcap", 1)
+    generate_random_packets("input.pcap", 1000)
+    # generate_random_packets("single_input.pcap", 1)
