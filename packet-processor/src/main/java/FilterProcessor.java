82dc59115f96125b9f45ca1e270475dd621889bd
Gordon <gl119@ic.ac.uk>
2022-06-01 16:57:45 +0100

    Created PacketProcessor interface (easier benchmarking) with a simple Filter implementation.

---
+++ packet-processor/src/main/java/FilterProcessor.java
@@ -0,0 +1,61 @@
+import components.PacketFilter;
+import components.PacketProducer;
+import components.PacketWriter;
+import io.pkts.packet.Packet;
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class FilterProcessor implements PacketProcessor {
+
+  private final Thread producerThread;
+  private final Thread filterThread;
+  private final Thread tcpThread;
+  private final Thread udpThread;
+
+  public FilterProcessor(int queueSize, String source, String tcpDest, String udpDest)
+      throws IOException {
+
+    final BlockingQueue<Packet> producerQueue = new ArrayBlockingQueue<>(queueSize);
+    final BlockingQueue<Packet> tcpQueue = new ArrayBlockingQueue<>(queueSize);
+    final BlockingQueue<Packet> udpQueue = new ArrayBlockingQueue<>(queueSize);
+
+    final PacketProducer packetProducer = new PacketProducer(source, producerQueue);
+    final PacketFilter packetFilter = new PacketFilter(producerQueue, tcpQueue, udpQueue);
+    final PacketWriter tcpWriter = new PacketWriter(tcpQueue, tcpDest);
+    final PacketWriter udpWriter = new PacketWriter(udpQueue, udpDest);
+
+    this.producerThread = new Thread(packetProducer);
+    this.filterThread = new Thread(packetFilter);
+    this.tcpThread = new Thread(tcpWriter);
+    this.udpThread = new Thread(udpWriter);
+  }
+
+  @Override
+  public void initialize() {
+    filterThread.start();
+    tcpThread.start();
+    udpThread.start();
+  }
+
+  @Override
+  public void start() throws InterruptedException {
+    producerThread.start();
+    producerThread.join();
+    filterThread.join();
+    tcpThread.join();
+    udpThread.join();
+  }
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    FilterProcessor processor = new FilterProcessor(
+        1000,
+        "src/main/resources/input.pcap",
+        "src/main/resources/tcp_output.pcap",
+        "src/main/resources/udp_output.pcap"
+    );
+
+    processor.initialize();
+    processor.start();
+  }
+}
--- packet-processor/src/main/java/PacketProcessor.java
+++ packet-processor/src/main/java/PacketProcessor.java
@@ -1,45 +1,7 @@
-import components.PacketFilter;
-import components.PacketProducer;
-import components.PacketWriter;
-import io.pkts.packet.Packet;
-import java.io.IOException;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+public interface PacketProcessor {
 
-public class PacketProcessor {
+  void initialize();
 
-  public static void simpleFilterPipeline() throws IOException, InterruptedException {
-    BlockingQueue<Packet> producerQueue = new ArrayBlockingQueue<>(100);
-    BlockingQueue<Packet> tcpQueue = new ArrayBlockingQueue<>(100);
-    BlockingQueue<Packet> udpQueue = new ArrayBlockingQueue<>(100);
-
-    final String source = "src/main/resources/input.pcap";
-    final String tcpDest = "src/main/resources/tcp_output.pcap";
-    final String udpDest = "src/main/resources/udp_output.pcap";
-
-    PacketProducer packetProducer = new PacketProducer(source, producerQueue);
-    PacketFilter packetFilter = new PacketFilter(producerQueue, tcpQueue, udpQueue);
-    PacketWriter tcpWriter = new PacketWriter(tcpQueue, tcpDest);
-    PacketWriter udpWriter = new PacketWriter(udpQueue, udpDest);
-
-    Thread producerThread = new Thread(packetProducer);
-    Thread filterThread = new Thread(packetFilter);
-    Thread tcpThread = new Thread(tcpWriter);
-    Thread udpThread = new Thread(udpWriter);
-
-    producerThread.start();
-    filterThread.start();
-    tcpThread.start();
-    udpThread.start();
-
-    producerThread.join();
-    filterThread.join();
-    tcpThread.join();
-    udpThread.join();
-  }
-
-  public static void main(String[] args) throws IOException, InterruptedException {
-    simpleFilterPipeline();
-  }
+  void start() throws InterruptedException;
 
 }
