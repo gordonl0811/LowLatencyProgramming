cfbda32a8c3b73758c9a20613b2defbc026c49a8
Gordon Lee <gordonl0811@gmail.com>
2022-06-06 14:14:50 +0100

    ForkJoin Benchmark class created.

---
+++ packet-processor/src/main/java/PacketProcessorBenchmarks/BenchmarkForkJoinProcessor.java
@@ -0,0 +1,83 @@
+package PacketProcessorBenchmarks;
+
+import PacketProcessor.DisruptorPacketProcessor.ForkJoinDisruptorProcessor;
+import PacketProcessor.DisruptorPacketProcessor.RewritePortDisruptorProcessor;
+import PacketProcessor.PacketProcessor;
+import PacketProcessor.QueuePacketProcessor.ForkJoinQueueProcessor;
+import PacketProcessor.QueuePacketProcessor.RewritePortQueueProcessor;
+import org.openjdk.jmh.annotations.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 3, time = 3)
+@Measurement(iterations = 1, time = 3)
+@Fork(value = 1)
+public class BenchmarkForkJoinProcessor {
+
+    @State(Scope.Benchmark)
+    public static class DisruptorImplementationState {
+
+        public PacketProcessor processor;
+
+        @Param({"8", "64", "512", "4096"})
+        public int bufferSize;
+
+        @Setup(Level.Invocation)
+        public void setup() throws IOException {
+            processor = new ForkJoinDisruptorProcessor(
+                    bufferSize,
+                    "src/main/resources/input_thousand.pcap",
+                    "src/main/resources/output/rewritten.pcap",
+                    12,
+                    34,
+                    56,
+                    78,
+                    1000
+            );
+            processor.initialize();
+        }
+
+        @TearDown(Level.Invocation)
+        public void teardown() {
+            processor.shutdown();
+        }
+    }
+
+    @Benchmark
+    public void benchmarkDisruptorImplementation(DisruptorImplementationState state) throws InterruptedException {
+        state.processor.start();
+    }
+
+    @State(Scope.Benchmark)
+    public static class QueueImplementationState {
+
+        public PacketProcessor processor;
+
+        @Param({"8", "64", "512", "4096"})
+        public int queueSize;
+
+        @Setup(Level.Invocation)
+        public void setup() throws IOException {
+            processor = new ForkJoinQueueProcessor(
+                    queueSize,
+                    "src/main/resources/input_thousand.pcap",
+                    "src/main/resources/output/rewritten.pcap",
+                    12,
+                    34,
+                    56,
+                    78,
+                    1000
+            );
+            processor.initialize();
+        }
+    }
+
+    @Benchmark
+    public void benchmarkQueueImplementation(QueueImplementationState state) throws InterruptedException {
+        state.processor.start();
+    }
+
+}
